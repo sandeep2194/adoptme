5cc5a48a249ad6b3462c1f8898a8d12b35feb8d8
sandeep2194 <sandeep2194@live.com>
2021-02-18 19:50:21 +0530

    TILL

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React from "react";
 
 import { render } from "react-dom";
-import Pet from "./Pet";
-
+import SearchParams from "./SearchParams";
 const App = () => {
-  // return React.createElement("div", { id: "id-attribute" }, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Snowy",
-  //     animal: "Dog",
-  //     breed: "Pomeranian",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Sheru",
-  //     animal: "Dog",
-  //     breed: "Mixed",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Shaahab",
-  //     animal: "Parrot",
-  //     breed: "Hushiyarpuriya",
-  //   }),
-  // ]);
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Snowy" animal="Dog" breed="pomerian" />
-      <Pet name="Pepper" animal="Bird" breed="Cocktail" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <SearchParams />
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  });
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            onChange={(event) => setLocation(event.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
