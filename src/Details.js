3e9dc609fa0071feb645075f53bc1dfb15facddc
sandeep2194 <sandeepsingh2194@gmail.com>
2021-02-26 10:42:24 +0530

    added error boundary

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundaries";
 
 class Details extends React.Component {
   state = { loading: true };
@@ -36,4 +37,10 @@ class Details extends React.Component {
   }
 }
 
-export default Details;
+export default function DetailsWithErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
